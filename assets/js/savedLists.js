d914342645898698cfdb73864c019a936df7763b
adamssef <adam33ef@gmail.com>
2020-03-04 20:51:34 +0100

    savelsavedLists has new functionalities: shoppingList toggle, also some minor frontend updates has been implemented

--- assets/js/savedLists.js
+++ assets/js/savedLists.js
@@ -30,16 +30,29 @@ class SavedLists extends Component {
     }
 
     showListItems = (e) => {
-        console.log(e.target);
-        let id = e.target.parentElement.id;
-        let listItems = this.state.data[id].listItems;
-        let listItemsOl = e.target.parentElement.getElementsByTagName('OL')[0];
-        for (let item in listItems) {
-            let li = document.createElement('li');
-            li.innerHTML = listItems[item];
-            listItemsOl.appendChild(li);
-        }
 
+        let listId = e.target.parentElement.nextSibling.children[0].id;
+        let list = document.getElementById(listId);
+
+        if (list.style.display === "") {
+            console.log(e.target.parentElement);
+            let id = e.target.parentElement.parentElement.id;
+            console.log(id);
+            let listItems = this.state.data[id].listItems;
+            let listItemsOl = e.target.parentElement.parentElement.getElementsByTagName('OL')[0];
+            for (let item in listItems) {
+                let li = document.createElement('li');
+                li.innerHTML = listItems[item];
+                listItemsOl.appendChild(li);
+            }
+
+            list.style.display = "block";
+        } else if (list.style.display === "block") {
+                list.style.display = "none";
+        } else {
+            list.style.display = "block"
+        }
+        ;
     }
 
 
@@ -49,17 +62,24 @@ class SavedLists extends Component {
 
         // console.log(callback);
 
-        return <div className={"listContainer"}>{this.state.data.map(function (item, index) {
-            return <div className="topDown5pxMargin listContainerEl" key={item.id}  id={index}>
-                <p className="inline right25pxMargin listContainerElParagraph">{item.id} {item.name}</p>
-                <button onClick={this.showListItems} className={"listContainerElButton"}>pokaż artykuły!</button>
-                <ol className="listContainerElElements" id="list"></ol>
-            </div>;
-        }, this)}</div>
+        return <div>
+            <h2>Twoje 10 ostatnio zapisanych list:</h2>
+            <div className={"listContainer"}>{this.state.data.map(function (item, index) {
+                return <div className="topDown5pxMargin listContainerEl" key={item.id} id={index}>
+                    <div className="listContainerElParagraph px10MarginLeft"><p
+                        className={"px10MarginUpDown"}> {index + 1} {item.name}</p></div>
+                    <div className={"listContainerElButton"}>
+                        <button className={"px10MarginUpDown"} onClick={this.showListItems}>pokaż listę!</button>
+                    </div>
+                    <div className="listContainerElElements">
+                        <ol id={`list` + (index + 1)}></ol>
+                    </div>
+                </div>;
+            }, this)}</div>
+        </div>
 
         // return <button onClick={()=>this.showListItems()}>Click me bitch</button>
     }
 }
-
 
 export default SavedLists;
