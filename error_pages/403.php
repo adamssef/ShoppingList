c6ad1d9e26a86a12e21528acbbac04312b7d8320
adamssef <adam33ef@gmail.com>
2020-03-09 12:03:25 +0100

    downgrading php required version to php 7.3

---
+++ error_pages/403.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+DUPA 403
+kutas
+<?php
+echo getcwd();
+echo $_SERVER['DOCUMENT_ROOT'];
+echo "dupa";
+?>
+
+
+</body>
+</html>
