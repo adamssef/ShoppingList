4c0472a28751c7cdadf0543b67ceae48f20d42ad
adamssef <adam33ef@gmail.com>
2020-05-03 12:22:57 +0200

    experimenting with routing

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ Encore
     // })
 
     // uncomment to define the assets of the project
-    .addEntry('app', ['babel-polyfill','./assets/js/app.js'])
+    .addEntry('index', ['babel-polyfill','./assets/js/index.js'])
     .configureLoaderRule('images', (loaderRule) => {
         loaderRule.options.esModule = false;
     })
