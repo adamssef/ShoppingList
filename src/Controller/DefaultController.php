fa4850556c2022f49dc95d6b38791d30ee1939cf
adamssef <adam33ef@gmail.com>
2020-05-16 17:37:00 +0200

    DefaultController now serialize the listItems before loading to DB

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -28,7 +28,61 @@ class DefaultController extends AbstractController
      */
     public function save(Request $request)
     {
+        if ($request->getMethod() === 'POST') {
+            $shoppingList = new ShoppingList();
+            $list = $request->request->all();
+        }
+            if (count($list) > 0) {
+                $em = $this->getDoctrine()->getManager();
+                $shoppingList->setCreationDate(new \DateTime());
+                $shoppingList->setName($list['name']);
+                unset($list['name']);
+                $list = serialize($list);
+                $shoppingList->setListItems($list);
+                $em->persist($shoppingList);
+                $em->flush();
+                $response = $this->json([]);
+                $response->headers->set('Content-Type', 'application/json');
+                return $response;
+            } elseif ($request->getMethod() === 'GET') {
+                return $this->redirect($request->server->get('HTTP_HOST'));
+            }
+        return new Response();
+    }
 
+    /**
+     * @Route("/saved", name="saved")
+     */
+    public function saved(Request $request)
+    {
+        if ($request->getMethod() === "POST") {
+            $em = $this->getDoctrine()->getManager();
+            $results = $em->getRepository(ShoppingList::class)->listAllShoppingLists();
+            $response = $this->json($results);
+
+            $response->headers->set('Content-Type', 'application/json');
+            $response->headers->set('Access-Control-Allow-Origin', "http://localhost:" . $request->getPort());
+            $response->headers->set('Access-Control-Allow-Methods', 'GET,POST');
+
+            return $response;
+        } else {
+            return $this->redirect("https://".$request->server->get('HTTP_HOST'));
+        }
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(Request $request)
+    {
+            return $this->redirect("https://".$request->server->get('HTTP_HOST'));
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(Request $request)
+    {
         if ($request->getMethod() === 'POST') {
             $shoppingList = new ShoppingList();
             $list = $request->request->all();
@@ -54,33 +108,7 @@ class DefaultController extends AbstractController
         return new Response();
     }
 
-    /**
-     * @Route("/saved", name="saved")
-     */
-    public function saved(Request $request)
-    {
-        if ($request->getMethod() === "POST") {
-            $em = $this->getDoctrine()->getManager();
-            $results = $em->getRepository(ShoppingList::class)->listAllShoppingLists();
 
-            $response = $this->json($results);
 
-            $response->headers->set('Content-Type', 'application/json');
-            $response->headers->set('Access-Control-Allow-Origin', "http://localhost:" . $request->getPort());
-            $response->headers->set('Access-Control-Allow-Methods', 'GET,POST');
-
-            return $response;
-        } else {
-            return $this->redirect("https://".$request->server->get('HTTP_HOST'));
-        }
-    }
-
-    /**
-     * @Route("/about", name="about")
-     */
-    public function about(Request $request)
-    {
-            return $this->redirect("https://".$request->server->get('HTTP_HOST'));
-    }
 
 }
