c829b2fdaa0b20b7f73f1e6998df41dfdf1b5bc5
adamssef <adam33ef@gmail.com>
2020-08-19 22:15:50 +0200

    /login endpoint has been developed so that very basic level of login functionality is now working

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Entity\ShoppingList;
 use App\Entity\User;
 use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Tests\Compiler\J;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,10 +14,19 @@ use Symfony\Component\HttpFoundation\Response as Response;
 use Symfony\Component\HttpFoundation\Request as Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Doctrine\DBAL\DBALException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class DefaultController extends AbstractController
 {
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
 
     /**
      * @Route("/")
@@ -92,18 +102,26 @@ class DefaultController extends AbstractController
     /**
      * @Route("/register", name="register")
      */
-    public function register(Request $request, Session $session)
+    public function register(Request $request, Session $session, EntityManagerInterface $em)
     {
         $session->start();
         $allowedOrigin = $_ENV['APP_ENV'] === 'dev' ? "https://localhost:" . $_SERVER['SERVER_PORT'] : "https://listazakupow.com.pl";
-
-        if ($request->getMethod() === "GET" && $request->headers->get('X-Custom-Header') !== 'regTokenRequest') {
+        $em = $this->getDoctrine()->getManager();
+        $regData = $request->request->all();
+        if (
+            $request->getMethod() === "GET" &&
+            $request->headers->get('X-Custom-Header') !== 'regTokenRequest'
+        ) {
             return $this->redirect($allowedOrigin);
         }
 
 
         //this scenario is accepted in a case where user agent renders the reg-form and needs a token to be generated
-        if ($request->getMethod() === "GET" && is_null($session->get('regToken')) && $request->headers->get('X-Custom-Header') === 'regTokenRequest') {
+        if (
+            $request->getMethod() === "GET" &&
+            is_null($session->get('regToken')) &&
+            $request->headers->get('X-Custom-Header') === 'regTokenRequest'
+        ) {
             $random = random_bytes(10);
             $token = md5($random);
             $session->set('regToken', $token);
@@ -112,21 +130,49 @@ class DefaultController extends AbstractController
             $jsonResponse->headers->set('Access-Control-Allow-Origin', $allowedOrigin);
             $jsonResponse->headers->set('Access-Control-Allow-Methods', 'GET');
             return $jsonResponse;
-        } elseif ($request->getMethod() === "GET" && !is_null($session->get('regToken'))) {
+        }
+
+        if (
+            $request->getMethod() === "GET" &&
+            !is_null($session->get('regToken'))
+        ) {
             return new JsonResponse($session->get('regToken'));
-        } elseif ($request->getMethod() === "POST" && $session->get('regToken') === $request->get('regToken') && $session->get('regToken') !== null && is_string($session->get('regToken'))) {
+        }
+
+        if (
+            $request->getMethod() === "POST" &&
+            $session->get('regToken') === $request->get('regToken') &&
+            $session->get('regToken') !== null &&
+            is_string($session->get('regToken'))
+        ) {
+
             $user = new User();
-            $regData = $request->request->all();
-            if (count($regData) > 0 && filter_var($regData['email'], FILTER_VALIDATE_EMAIL) && ctype_alnum($regData['fName'])) {
-                $em = $this->getDoctrine()->getManager();
+            if (
+                count($regData) > 0 &&
+                filter_var($regData['email'], FILTER_VALIDATE_EMAIL) !== false &&
+                isset($regData['fName']) &&
+                isset($regData['password'])
+            ) {
+                $isUserNew = $em->getRepository(User::class)->findOneBy(['email' => $request->request->all()['email']]) === null;
+                if ($isUserNew === false) {
+                    $response = $this->json("user already exists");
+                    $response->headers->set('Access-Control-Allow-Origin', $allowedOrigin);
+                    $response->headers->set('Content-Type', 'application/json');
+                    $response->headers->set('Access-Control-Allow-Methods', 'POST');
+                    $session->invalidate();
+                    return $response;
+                }
                 $user->setRegDate();
                 $user->setFirstName($regData['fName']);
                 $user->setEmail(filter_var($regData['email'], FILTER_SANITIZE_EMAIL));
-                $user->setPassword($regData['password']);
+                $user->setPassword($this->passwordEncoder->encodePassword(
+                    $user,
+                    $regData['password']
+                ));
+
 
                 $em->persist($user);
                 $em->flush();
-
                 $response = $this->json("registration successful");
                 $response->headers->set('Access-Control-Allow-Origin', $allowedOrigin);
                 $response->headers->set('Content-Type', 'application/json');
@@ -135,8 +181,58 @@ class DefaultController extends AbstractController
                 return $response;
             }
         }
-        return new JsonResponse("null");
+        return new JsonResponse($request->request->all());
     }
 
 
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, Session $session, EntityManagerInterface $em)
+    {
+        $allowedOrigin = $_ENV['APP_ENV'] === 'dev' ? "https://localhost:" . $_SERVER['SERVER_PORT'] : "https://listazakupow.com.pl";
+        $session->start();
+        if (
+            $request->getMethod() === "GET" &&
+            is_null($session->get('logToken')) &&
+            $request->headers->get('X-Custom-Header') === 'logTokenRequest'
+        ) {
+            $random = random_bytes(10);
+            $token = md5($random);
+            $session->set('logToken', $token);
+            $jsonResponse = new JsonResponse($token);
+            $jsonResponse->headers->set('Content-Type', 'application/json');
+            $jsonResponse->headers->set('Access-Control-Allow-Origin', $allowedOrigin);
+            $jsonResponse->headers->set('Access-Control-Allow-Methods', 'GET');
+            return $jsonResponse;
+        }
+
+        if ($request->getMethod() !== "POST") {
+            return $this->redirect($allowedOrigin);
+        }
+
+
+        $em = $this->getDoctrine()->getManager();
+        $logData = $request->request->all();
+        if (
+            $request->getMethod() === "POST" &&
+            count($logData) > 0 &&
+            filter_var($logData['email'], FILTER_VALIDATE_EMAIL) !== false &&
+            isset($logData['password'])
+        ) {
+            $user = $em->getRepository(User::Class)->findOneBy(['email' => $logData['email']]);
+            $email = $user === null ? null : $user->getEmail();
+            if ($email !== null AND $this->passwordEncoder->isPasswordValid($user, $logData['password'])) {
+                return new JsonResponse('Login successful');
+            } else {
+                return new JsonResponse('Login unsuccessful');
+            }
+        }  else {
+            return new JsonResponse('Login unsuccessful');
+        }
+
+
+    }
 }
+
+
