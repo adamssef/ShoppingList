6c0b53e58fba57ea94a78fbf404ef81904e54f32
adamssef <adam33ef@gmail.com>
2020-12-20 11:30:58 +0100

    Logo component separated as a standalone component

---
+++ assets/js/homepage/logo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**
+ * Experimenting with hook..
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Logo() {
+    return (
+        <h2 className={'logo__h2'}>
+            <span className={'logo-span--color'}>lista</span>zakupow.<span className={'logo-span-color'}>com</span>.pl
+        </h2>
+    )
+}
+
+export default Logo;
